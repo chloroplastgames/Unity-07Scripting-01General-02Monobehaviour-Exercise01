c5de4dcaf86707e083744c0e2c8b3a3e60c6b6a6
david-rodriguez-levelup <72571654+david-rodriguez-levelup@users.noreply.github.com>
2020-10-10 13:38:46 +0200

    First draft
    Plugins folder created.
    SimpleLibrary.dll added to Plugins folder.
    CanvasController script added to Canvas object.

---
+++ Assets/Scripts/CanvasController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using SimpleLibrary;
+
+public class CanvasController : MonoBehaviour
+{
+    private const string UNKNOWN_OPERATION = "UNKNOWN OPERATION";
+    private const string ERROR = "ERROR";
+
+    private const int ADD = 0;
+    private const int SUBSTRACT = 1;
+    private const int MULTIPLY = 2;
+    private const int DIVIDE = 3;
+    
+    // Inspector
+    public InputField input1;
+    public InputField input2;
+    public Dropdown operation;
+    public Text result;
+
+    public void Compute()
+    {
+        try
+        {
+            float factor1 = float.Parse(input1.text);
+            float factor2 = float.Parse(input2.text);
+            switch (operation.value)
+            {
+                case ADD:
+                    result.text = SimpleMath.Add(factor1, factor2).ToString();
+                    break;
+                case SUBSTRACT:
+                    result.text = SimpleMath.Substract(factor1, factor2).ToString();
+                    break;
+                case MULTIPLY:
+                    result.text = SimpleMath.Multiply(factor1, factor2).ToString();
+                    break;
+                case DIVIDE:
+                    result.text = SimpleMath.Divide(factor1, factor2).ToString();
+                    break;
+                default:
+                    result.text = UNKNOWN_OPERATION;
+                    break;
+            }
+        } catch (FormatException)
+        {
+            result.text = ERROR;
+        } 
+
+    }
+
+}
