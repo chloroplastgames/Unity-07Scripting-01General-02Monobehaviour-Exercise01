97ee9deee134d2788ab86e840a8d82d3e9200150
Soncan <andreu.kwazema.levelup@gmail.com>
2020-10-13 14:30:53 +0200

    MathLibrary (Visual Studio)
    - Visual study project with the MathLibrary class
    
    Signed-off-by: Soncan <andreu.kwazema.levelup@gmail.com>

---
+++ VisualStudio/ClassLibraryProjects/MathLibrary/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    public class MyMath
+    {
+        // Puedo crear una aplicación en consola? Despues puede dar problemas para explicar la clase a Unity?
+
+        public static float Add(float a, float b)  => a + b;
+
+        public static float Divide(float a, float b) => a / b;
+
+        public static float Subtract(float a, float b) => a - b;
+
+        public static float Multiply(float a, float b) => a * b;
+    }
+}
