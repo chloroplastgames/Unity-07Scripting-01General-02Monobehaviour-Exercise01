066d83875ba7c1990f4d48bea46b3d41adb3a901
rafaelalma92 <rafaelalma92@gmail.com>
2020-10-13 21:07:17 +0200

    Add class library

---
+++ BasicOperations/BasicOperations/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BasicOperationsLibrary
+{
+    public class BasicOperations
+    {
+        public float Addition(float a, float b)
+        {
+            return a + b;
+        }
+
+        public float Subtraction(float a, float b)
+        {
+            return a - b;
+        }
+
+        public float Multiplication(float a, float b)
+        {
+            return a * b;
+        }
+
+        public float Division(float a, float b)
+        {
+            return a / b;
+        }
+    }
+}
